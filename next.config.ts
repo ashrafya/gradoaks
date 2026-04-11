4e349e1ee05396a647aee4630b419c97fb6323cc
Yawar Ashraf <19ya1@QSE-C34CL7N31V.local>
2026-04-11 17:42:13 -0400

    deploy: update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/gradoaks' : '',
+  basePath: '',
   images: {
     unoptimized: true,
     remotePatterns: [
